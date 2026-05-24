bc23044921356e7370e8a60352ec0b38891d4d81
Fedesan14 <fede14.sanchez@gmail.com>
2026-05-23 21:36:01 -0300

    [FTR][MS-12]: add react router and first pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { InstallButton } from './components/installButton'
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
 
 function App() {
 
 
   return (
     <>
+      <div 
+        className="min-h-screen bg-base-100 flex items-center justify-center"
+      >
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+      </div>
       <InstallButton />
     </>
   )
